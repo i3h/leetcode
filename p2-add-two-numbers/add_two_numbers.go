1343622a5dc7fb878958b4f28d9d077ea555825a
noobly314 <noobly314@gmail.com>
2019-12-25 19:42:36 +0800

    finish p2

--- p2-add-two-numbers/add_two_numbers.go
+++ p2-add-two-numbers/add_two_numbers.go
@@ -1,12 +1,39 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l := &ListNode{Val: 0, Next: nil}
+	p := l
+	v1 := 0
+	v2 := 0
+	quo := 0
+	p1 := l1
+	p2 := l2
 
+	for {
+		if p1 == nil && p2 == nil {
+			if quo != 0 {
+				s := &ListNode{Val: quo, Next: nil}
+				p.Next = s
+			}
+			break
+		}
+		if p1 != nil {
+			v1 = p1.Val
+			p1 = p1.Next
+		} else {
+			v1 = 0
+		}
+		if p2 != nil {
+			v2 = p2.Val
+			p2 = p2.Next
+		} else {
+			v2 = 0
+		}
+		s := &ListNode{Val: (v1 + v2 + quo) % 10, Next: nil}
+		quo = (v1 + v2 + quo) / 10
+		p.Next = s
+		p = s
+	}
+
+	return l.Next
 }
--- p2-add-two-numbers/main.go
+++ p2-add-two-numbers/main.go
@@ -2,6 +2,47 @@ package main
 
 import "fmt"
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func main() {
-	fmt.Println("vim-go")
+	l1 := createList(1)
+	l2 := createList(9)
+	printList(l1)
+	fmt.Println()
+	printList(l2)
+	fmt.Println()
+	l3 := addTwoNumbers(l1, l2)
+	printList(l3)
+	fmt.Println()
+}
+
+func createList(nums ...int) *ListNode {
+	var l, p *ListNode
+
+	for i, n := range nums {
+		if i == 0 {
+			p = &ListNode{Val: n, Next: nil}
+			l = p
+		} else {
+			p.Next = &ListNode{Val: n, Next: nil}
+			p = p.Next
+		}
+	}
+
+	return l
+}
+
+func printList(p *ListNode) {
+	for {
+		if p.Next == nil {
+			fmt.Println(p.Val)
+			return
+		} else {
+			fmt.Println(p.Val)
+			p = p.Next
+		}
+	}
 }
