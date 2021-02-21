30f76c52d5e1e83683eb1cbe2e7ff211e5d19a5d
noobly314 <noobly314@gmail.com>
2021-02-21 20:30:00 +0800

    finish p15

--- p15-3sum/main.go
+++ p15-3sum/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	//nums := []int{-1, 0, 1, 2, -1, -4}
-	nums := []int{0, 0, 0, 0}
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	//nums := []int{0, 0, 0, 0}
 	fmt.Println("Input: ", nums)
 
 	set := threeSum(nums)
