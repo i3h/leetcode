877680e07393040bf1d341f2810ddfed4da0851e
noobly314 <noobly314@gmail.com>
2021-02-21 19:51:38 +0800

    update p15

--- p15-3sum/sum.go
+++ p15-3sum/sum.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"sort"
+	"strconv"
 )
 
 func threeSum(nums []int) [][]int {
-	var set [][]int
+	var set, tmp_set [][]int
 	sort.Ints(nums)
 	//fmt.Println("Sort: ", nums)
 
@@ -13,23 +15,53 @@ func threeSum(nums []int) [][]int {
 		if i > 0 && nums[i-1] == nums[i] {
 			continue
 		}
+		var s []int
 		for j := i + 1; j < len(nums); j++ {
-			if j > i+1 && nums[j-1] == nums[j] {
-				continue
-			}
-			for k := j + 1; k < len(nums); k++ {
-				if k > j+1 && nums[k-1] == nums[k] {
-					continue
-				}
-				if nums[i]+nums[j]+nums[k] == 0 {
-					//fmt.Println(i+1, j+1, k+1)
-					set = append(set, []int{nums[i], nums[j], nums[k]})
-				}
-				if nums[i]+nums[j]+nums[k] > 0 {
-					break
-				}
+			if j != i {
+				s = append(s, nums[j])
 			}
 		}
+		tmp_set = append(tmp_set, twoSum(s, -nums[i])...)
+	}
+
+	set = cleanSet(tmp_set)
+
+	return set
+}
+
+func cleanSet(set [][]int) [][]int {
+	var new_set [][]int
+
+	m := make(map[string][]int)
+	for _, t := range set {
+		fmt.Println(t)
+		s := strconv.Itoa(t[0]) + "+" + strconv.Itoa(t[1]) + "+" + strconv.Itoa(t[2])
+		m[s] = t
+		fmt.Println(s)
+	}
+	fmt.Println(m)
+
+	for _, v := range m {
+		new_set = append(new_set, v)
+	}
+
+	return new_set
+}
+
+func twoSum(nums []int, target int) [][]int {
+	var set [][]int
+
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		com := target - nums[i]
+		if val, ok := m[com]; ok && i != val {
+			set = append(set, []int{-target, nums[i], nums[val]})
+			//break
+		}
+		m[nums[i]] = i
+	}
+	for i := 0; i < len(set); i++ {
+		sort.Ints(set[i])
 	}
 
 	return set
