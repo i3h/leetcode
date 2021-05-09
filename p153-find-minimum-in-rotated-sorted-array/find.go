538c4bec73ce637963dd8fe5e362d03af88841a9
noobly314 <noobly314@gmail.com>
2021-05-09 21:47:22 +0800

    finish p153

---
+++ p153-find-minimum-in-rotated-sorted-array/find.go
@@ -0,0 +1,11 @@
+package main
+
+func findMin(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			return nums[i]
+		}
+	}
+
+	return nums[0]
+}
---
+++ p153-find-minimum-in-rotated-sorted-array/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 4, 5, 1, 2}
+	fmt.Println(nums)
+	fmt.Println(findMin(nums))
+}
