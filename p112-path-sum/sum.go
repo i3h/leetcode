886647f658826e7dee8e0ebead9647147b84318d
noobly314 <noobly314@gmail.com>
2021-05-10 22:30:11 +0800

    finish p112

---
+++ p112-path-sum/sum.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	} else {
+		return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+	}
+}
